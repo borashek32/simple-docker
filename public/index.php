4f5867717dcb2fbdf47591ab9daba17f6d5f840e
Nataly Baranova <borashek@inbox.ru>
2024-12-27 12:58:32 +0300

    init

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Docker</title>
+</head>
+<body>
+  <h1>Hello docker1234</h1>
+  <?php echo 1324 ?>
+</body>
+</html>
